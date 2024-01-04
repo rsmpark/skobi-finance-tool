dea29f9db979fafd51195f5fe3dbf95cf3c411bd
Rick Park <ricksmp95@gmail.com>
2024-01-04 13:00:17 +0000

    add currency mask to currnecy inputs

--- src/components/currencyInput/CurrencyInput.tsx
+++ src/components/currencyInput/CurrencyInput.tsx
@@ -2,76 +2,67 @@ import MuiAttachMoneyRoundedIcon from "@mui/icons-material/AttachMoneyRounded";
 import MuiInputAdornment from "@mui/material/InputAdornment";
 import MuiTextField from "@mui/material/TextField";
 import MuiTypography from "@mui/material/Typography";
-import { ChangeEvent, FC } from "react";
+import { ChangeEvent, FC, forwardRef } from "react";
+import MaskedInput, { MaskedInputProps } from "react-text-mask";
+import { createNumberMask } from "text-mask-addons";
 
-import { Props } from "./CurrencyInput.types";
+import { CurrnecyInputProps } from "./CurrencyInput.types";
 
-const CurrencyInput: FC<Props> = ({ value, setValue, ...props }) => {
-  // const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-  //   validateValue(e.target.value);
-  // };
+const defaultMaskOptions = {
+  prefix: "$",
+  suffix: "",
+  includeThousandsSeparator: true,
+  thousandsSeparatorSymbol: ",",
+  allowDecimal: true,
+  decimalSymbol: ".",
+  decimalLimit: 2,
+  allowNegative: false,
+  allowLeadingZeroes: false,
+};
 
-  // const validateValue = (inputValue: string) => {
-  //   // Remove non-digit characters from the input value
-  //   const numericValue = inputValue.replace(/[^0-9]/g, "");
+const MuiInput = forwardRef(({ name, placeholder, onChange }: MaskedInputProps, ref) => {
+  return (
+    <MuiTextField
+      inputRef={ref}
+      name={name}
+      size="small"
+      placeholder={placeholder}
+      fullWidth
+      sx={{
+        input: { textAlign: "right" },
+      }}
+      onChange={onChange}
+      InputProps={{
+        startAdornment: (
+          <MuiInputAdornment position="start">
+            <MuiAttachMoneyRoundedIcon sx={{ color: "hsl(184, 14%, 56%)" }} />
+          </MuiInputAdornment>
+        ),
+      }}
+    />
+  );
+});
 
-  //   // Format the numeric value as a currency string
-  //   const formattedValue = new Intl.NumberFormat("en-US", {
-  //     style: "currency",
-  //     currency: "USD",
-  //   }).format(Number(numericValue) / 100);
-
-  //   setValue(formattedValue);
-  // };
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    // Remove non-numeric characters except for the first period
-    const numericString = e.target.value.replace(/[^\d.]/g, "");
-    console.log(
-      "🚀 ~ file: currencyInput.tsx:30 ~ handleChange ~ numericString:",
-      numericString
-    );
-
-    // Ensure only one period is present
-    const periodIndex = numericString.indexOf(".");
-    if (periodIndex !== -1) {
-      const integerPart = numericString.slice(0, periodIndex);
-      const decimalPart = numericString.slice(periodIndex + 1).replace(/\./g, "");
-      const formattedValue = parseFloat(`${integerPart}.${decimalPart}`).toLocaleString(
-        "en-US",
-        {
-          minimumFractionDigits: 0,
-          maximumFractionDigits: Math.min(decimalPart.length, 2),
-        }
-      );
-      setValue(formattedValue);
-    } else {
-      const formattedValue = parseFloat(numericString).toLocaleString("en-US");
-      setValue(formattedValue);
-    }
-  };
+const CurrencyInput: FC<CurrnecyInputProps> = ({ maskOptions, setValue, ...props }) => {
+  const currencyMask = createNumberMask({
+    ...defaultMaskOptions,
+    ...maskOptions,
+  });
 
   return (
     <>
       <MuiTypography variant="subtitle2" gutterBottom>
         {props.label}
       </MuiTypography>
-      <MuiTextField
-        name={props.name}
-        size="small"
+      <MaskedInput
+        mask={currencyMask}
         placeholder="0"
-        value={value}
-        fullWidth
-        sx={{
-          input: { textAlign: "right" },
+        id={props.name}
+        onChange={(e: ChangeEvent<HTMLInputElement>) => {
+          setValue(e.target.value);
         }}
-        onChange={handleChange}
-        InputProps={{
-          startAdornment: (
-            <MuiInputAdornment position="start">
-              <MuiAttachMoneyRoundedIcon sx={{ color: "hsl(184, 14%, 56%)" }} />
-            </MuiInputAdornment>
-          ),
+        render={(ref, props) => {
+          return <MuiInput mask={false} ref={ref} {...props} />;
         }}
       />
     </>
--- src/components/currencyInput/CurrencyInput.types.ts
+++ src/components/currencyInput/CurrencyInput.types.ts
@@ -1,6 +1,23 @@
 import { StandardTextFieldProps } from "@mui/material";
 
-export interface Props extends StandardTextFieldProps {
-  value: string;
+export interface NumberMaskOptions {
+  prefix: string;
+  suffix: string;
+  includeThousandsSeparator: boolean;
+  thousandsSeparatorSymbol: string;
+  allowDecimal: boolean;
+  decimalSymbol: string;
+  decimalLimit: number;
+  requireDecimal: boolean;
+  allowNegative: boolean;
+  allowLeadingZeroes: boolean;
+  integerLimit: number;
+}
+export interface CurrnecyInputProps extends StandardTextFieldProps {
+  maskOptions?: NumberMaskOptions;
   setValue: (value: string) => void;
+}
+
+export interface MaskInputRenderProps {
+  inputRef: React.Ref<HTMLInputElement>;
 }
