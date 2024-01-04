cfb997f13e30f717984e4a7f80c28ef2d223c953
Rick Park <ricksmp95@gmail.com>
2024-01-04 19:35:01 +0000

    add calculate button

--- src/components/tipSummary/TipSummary.tsx
+++ src/components/tipSummary/TipSummary.tsx
@@ -9,17 +9,25 @@ const TipSummary = () => {
     <MuiStack
       spacing={3}
       justifyContent="space-between"
-      sx={{ p: 4, width: "100%", bgcolor: "primary.dark", borderRadius: 3 }}
+      sx={{ p: 4, width: "400px", bgcolor: "primary.dark", borderRadius: 3 }}
     >
       <TipSummaryContent />
 
-      <MuiBox>
+      <MuiBox display="flex" justifyContent="space-between">
         <MuiButton
           type="reset"
           variant="contained"
-          fullWidth
           // disabled={!bill && !tip && !people}
-          sx={{ color: "primary.dark", bgcolor: "primary.main" }}
+          sx={{ color: "primary.dark", bgcolor: "primary.main", flexGrow: 1, mx: 1 }}
+          // onClick={handleReset}
+        >
+          Calculate
+        </MuiButton>
+        <MuiButton
+          type="reset"
+          variant="contained"
+          // disabled={!bill && !tip && !people}
+          sx={{ color: "primary.dark", bgcolor: "primary.main", mx: 1, px: 3 }}
           // onClick={handleReset}
         >
           Reset
