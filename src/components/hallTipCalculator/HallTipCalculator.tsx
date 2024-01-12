e3be120926c36cb9e7b08172d2a779776d9ca7f4
Rick Park <ricksmp95@gmail.com>
2024-01-12 14:31:20 +0000

    add mui data grid

--- src/components/hallTipCalculator/HallTipCalculator.tsx
+++ src/components/hallTipCalculator/HallTipCalculator.tsx
@@ -1,6 +1,8 @@
 import MuiPaper from "@mui/material/Paper";
 import MuiStack from "@mui/material/Stack";
 
+import HallTipSummary from "@features/hallTipSummary/HallTipSummary";
+
 const HallTipCalculator = () => {
   return (
     <MuiPaper
@@ -8,7 +10,9 @@ const HallTipCalculator = () => {
       elevation={1}
       sx={{ maxWidth: 800, padding: 4, borderRadius: 4 }}
     >
-      <MuiStack direction={{ xs: "column", md: "row" }} spacing={5}></MuiStack>
+      <MuiStack direction={{ xs: "column", md: "row" }} spacing={5}>
+        <HallTipSummary />
+      </MuiStack>
     </MuiPaper>
   );
 };
