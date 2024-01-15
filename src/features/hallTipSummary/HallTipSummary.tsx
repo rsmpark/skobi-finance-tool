0472d9e904ffeaf301c13c3cb96e4ff3a4212b4a
Rick Park <ricksmp95@gmail.com>
2024-01-15 21:40:06 +0000

    add mock data and toolbar

--- src/features/hallTipSummary/HallTipSummary.tsx
+++ src/features/hallTipSummary/HallTipSummary.tsx
@@ -1,70 +1,101 @@
-import Box from "@mui/material/Box";
-import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+import MuiBox from "@mui/material/Box";
+import MuiButton from "@mui/material/Button";
+import {
+  DataGrid,
+  GridAddIcon,
+  GridColDef,
+  GridRowModes,
+  GridRowModesModel,
+  GridRowsProp,
+  GridToolbarContainer,
+} from "@mui/x-data-grid";
+import { random, randomId } from "@mui/x-data-grid-generator";
 
-const columns: GridColDef[] = [
-  { field: "id", headerName: "ID", width: 90 },
-  {
-    field: "firstName",
-    headerName: "First name",
-    width: 150,
-    editable: true,
-  },
-  {
-    field: "lastName",
-    headerName: "Last name",
-    width: 150,
-    editable: true,
-  },
-  {
-    field: "age",
-    headerName: "Age",
-    type: "number",
-    width: 110,
-    editable: true,
-  },
-  {
-    field: "fullName",
-    headerName: "Full name",
-    description: "This column has a value getter and is not sortable.",
-    sortable: false,
-    width: 160,
-    valueGetter: (params: GridValueGetterParams) =>
-      `${params.row.firstName || ""} ${params.row.lastName || ""}`,
-  },
-];
+const DAYS = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
+const generateColumns = () => {
+  const columns: GridColDef[] = [
+    {
+      field: "name",
+      headerName: "Name",
+      width: 120,
+      editable: true,
+      renderHeader: (params) => {
+        return <b>{params.colDef.headerName}</b>;
+      },
+    },
+  ];
+
+  DAYS.forEach((day) => {
+    columns.push({
+      field: day.toLowerCase(),
+      flex: 1,
+      headerName: day,
+      type: "number",
+      editable: true,
+      sortable: false,
+      headerAlign: "center",
+      align: "center",
+      renderHeader: (params) => {
+        return <b>{params.colDef.headerName}</b>;
+      },
+    });
+  });
+
+  return columns;
+};
+
+const columns: GridColDef[] = generateColumns();
 
 const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 14 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 31 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 31 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 11 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+  {
+    id: randomId(),
+    name: "John",
+    mon: 1,
+    tue: 2,
+    wed: 3,
+    thu: 4,
+    fri: 5,
+    sat: 6,
+    sun: 7,
+  },
 ];
+
+interface EditToolbarProps {
+  setRows: (newRows: (oldRows: GridRowsProp) => GridRowsProp) => void;
+  setRowModesModel: (
+    newModel: (oldModel: GridRowModesModel) => GridRowModesModel
+  ) => void;
+}
+
+function EditToolbar(props: EditToolbarProps) {
+  const { setRows, setRowModesModel } = props;
+
+  const handleClick = () => {
+    const id = randomId();
+    setRows((oldRows) => [...oldRows, { id, name: "", age: "", isNew: true }]);
+    setRowModesModel((oldModel) => ({
+      ...oldModel,
+      [id]: { mode: GridRowModes.Edit, fieldToFocus: "name" },
+    }));
+  };
+
+  return (
+    <GridToolbarContainer>
+      <MuiButton color="primary" startIcon={<GridAddIcon />} onClick={handleClick}>
+        Add record
+      </MuiButton>
+    </GridToolbarContainer>
+  );
+}
 
 const HallTipSummary = () => {
   // TODO: Example borrowed from https://mui.com/x/react-data-grid/
   // Require modification
   return (
-    <Box sx={{ height: 400, width: "100%" }}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: {
-              pageSize: 5,
-            },
-          },
-        }}
-        pageSizeOptions={[5]}
-        checkboxSelection
-        disableRowSelectionOnClick
-      />
-    </Box>
+    <MuiBox sx={{ height: 400, width: "100%" }}>
+      <DataGrid rows={rows} columns={columns} disableColumnSelector disableColumnMenu />
+    </MuiBox>
   );
 };
 
