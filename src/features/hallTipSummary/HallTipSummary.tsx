5d393255bfdd1a78cc5e4df7479b005855184f8a
Rick Park <ricksmp95@gmail.com>
2024-01-16 20:10:11 +0000

    add delete and add funtionality

--- src/features/hallTipSummary/HallTipSummary.tsx
+++ src/features/hallTipSummary/HallTipSummary.tsx
@@ -6,79 +6,15 @@ import {
   GridAddIcon,
   GridColDef,
   GridDeleteIcon,
-  GridRowModes,
+  GridRowId,
   GridRowModesModel,
   GridRowsProp,
   GridToolbarContainer,
 } from "@mui/x-data-grid";
-import { random, randomId } from "@mui/x-data-grid-generator";
+import { randomId } from "@mui/x-data-grid-generator";
+import { useState } from "react";
 
 const DAYS = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-
-const generateColumns = () => {
-  const columns: GridColDef[] = [
-    {
-      field: "name",
-      headerName: "Name",
-      width: 120,
-      editable: true,
-      renderHeader: (params) => {
-        return <b>{params.colDef.headerName}</b>;
-      },
-    },
-  ];
-
-  DAYS.forEach((day) => {
-    columns.push({
-      field: day.toLowerCase(),
-      flex: 1,
-      headerName: day,
-      type: "number",
-      editable: true,
-      sortable: false,
-      headerAlign: "center",
-      align: "center",
-      renderHeader: (params) => {
-        return <b>{params.colDef.headerName}</b>;
-      },
-    });
-  });
-
-  columns.push({
-    field: "actions",
-    type: "actions",
-    width: 20,
-    cellClassName: "actions",
-    getActions: ({ id }) => {
-      return [
-        <GridActionsCellItem
-          icon={<GridDeleteIcon />}
-          label="Delete"
-          onClick={() => console.log(`Deleting row with id: ${id}`)}
-          color="inherit"
-        />,
-      ];
-    },
-  });
-
-  return columns;
-};
-
-const columns: GridColDef[] = generateColumns();
-
-const rows = [
-  {
-    id: randomId(),
-    name: "John",
-    mon: 1,
-    tue: 2,
-    wed: 3,
-    thu: 4,
-    fri: 5,
-    sat: 6,
-    sun: 7,
-  },
-];
 
 interface EditToolbarProps {
   setRows: (newRows: (oldRows: GridRowsProp) => GridRowsProp) => void;
@@ -87,33 +23,110 @@ interface EditToolbarProps {
   ) => void;
 }
 
-function EditToolbar(props: EditToolbarProps) {
-  const { setRows, setRowModesModel } = props;
+const HallTipSummary = () => {
+  const EditToolbar = (props: EditToolbarProps) => {
+    const { setRows } = props;
 
-  const handleClick = () => {
-    const id = randomId();
-    setRows((oldRows) => [...oldRows, { id, name: "", age: "", isNew: true }]);
-    setRowModesModel((oldModel) => ({
-      ...oldModel,
-      [id]: { mode: GridRowModes.Edit, fieldToFocus: "name" },
-    }));
+    const handleClick = () => {
+      const id = randomId();
+      setRows((oldRows) => [
+        ...oldRows,
+        { id, name: "", mon: 0, tue: 0, wed: 0, thu: 0, fri: 0, sat: 0, sun: 0 },
+      ]);
+    };
+
+    return (
+      <GridToolbarContainer>
+        <MuiButton color="primary" onClick={handleClick}>
+          <GridAddIcon />
+        </MuiButton>
+      </GridToolbarContainer>
+    );
+  };
+  const generateColumns = () => {
+    const columns: GridColDef[] = [
+      {
+        field: "name",
+        headerName: "Name",
+        width: 120,
+        editable: true,
+        renderHeader: (params) => {
+          return <b>{params.colDef.headerName}</b>;
+        },
+      },
+    ];
+
+    DAYS.forEach((day) => {
+      columns.push({
+        field: day.toLowerCase(),
+        flex: 1,
+        headerName: day,
+        type: "number",
+        editable: true,
+        sortable: false,
+        headerAlign: "center",
+        align: "center",
+        renderHeader: (params) => {
+          return <b>{params.colDef.headerName}</b>;
+        },
+      });
+    });
+
+    columns.push({
+      field: "actions",
+      type: "actions",
+      width: 20,
+      cellClassName: "actions",
+      getActions: ({ id }) => {
+        return [
+          <GridActionsCellItem
+            icon={<GridDeleteIcon />}
+            label="Delete"
+            onClick={handleDeleteClick(id)}
+            color="inherit"
+          />,
+        ];
+      },
+    });
+
+    return columns;
+  };
+
+  const columns: GridColDef[] = generateColumns();
+
+  const initialRows = [
+    {
+      id: randomId(),
+      name: "John",
+      mon: 1,
+      tue: 2,
+      wed: 3,
+      thu: 4,
+      fri: 5,
+      sat: 6,
+      sun: 7,
+    },
+  ];
+  const [rows, setRows] = useState(initialRows);
+
+  const handleDeleteClick = (id: GridRowId) => () => {
+    setRows(rows.filter((row) => row.id !== id));
   };
 
   return (
-    <GridToolbarContainer>
-      <MuiButton color="primary" startIcon={<GridAddIcon />} onClick={handleClick}>
-        Add record
-      </MuiButton>
-    </GridToolbarContainer>
-  );
-}
-
-const HallTipSummary = () => {
-  // TODO: Example borrowed from https://mui.com/x/react-data-grid/
-  // Require modification
-  return (
     <MuiBox sx={{ height: 400, width: "100%" }}>
-      <DataGrid rows={rows} columns={columns} disableColumnSelector disableColumnMenu />
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        slots={{
+          toolbar: EditToolbar,
+        }}
+        slotProps={{
+          toolbar: { setRows },
+        }}
+        disableColumnSelector
+        disableColumnMenu
+      />
     </MuiBox>
   );
 };
