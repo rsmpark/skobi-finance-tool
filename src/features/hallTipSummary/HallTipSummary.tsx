6321492a220524c28bed66aed81c6e553af36293
Rick Park <ricksmp95@gmail.com>
2024-01-18 21:04:59 +0000

    add mock data

--- src/features/hallTipSummary/HallTipSummary.tsx
+++ src/features/hallTipSummary/HallTipSummary.tsx
@@ -1,219 +1,223 @@
 import MuiBox from "@mui/material/Box";
+import { useReducer } from "react";
 
 import Table from "@components/table/Table";
+import makeData from "@util/table.util";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "add_option_to_column": {
+      const optionIndex = state.columns.findIndex(
+        (column) => column.id === action.columnId
+      );
+      return {
+        ...state,
+        skipReset: true,
+        columns: [
+          ...state.columns.slice(0, optionIndex),
+          {
+            ...state.columns[optionIndex],
+            options: [
+              ...state.columns[optionIndex].options,
+              { label: action.option, backgroundColor: action.backgroundColor },
+            ],
+          },
+          ...state.columns.slice(optionIndex + 1, state.columns.length),
+        ],
+      };
+    }
+    case "add_row":
+      return {
+        ...state,
+        skipReset: true,
+        data: [...state.data, {}],
+      };
+    case "update_column_type": {
+      const typeIndex = state.columns.findIndex(
+        (column) => column.id === action.columnId
+      );
+      switch (action.dataType) {
+        case "number":
+          if (state.columns[typeIndex].dataType === "number") {
+            return state;
+          } else {
+            return {
+              ...state,
+              columns: [
+                ...state.columns.slice(0, typeIndex),
+                { ...state.columns[typeIndex], dataType: action.dataType },
+                ...state.columns.slice(typeIndex + 1, state.columns.length),
+              ],
+              data: state.data.map((row) => ({
+                ...row,
+                [action.columnId]: isNaN(row[action.columnId])
+                  ? ""
+                  : Number.parseInt(row[action.columnId]),
+              })),
+            };
+          }
+        case "select":
+          if (state.columns[typeIndex].dataType === "select") {
+            return {
+              ...state,
+              columns: [
+                ...state.columns.slice(0, typeIndex),
+                { ...state.columns[typeIndex], dataType: action.dataType },
+                ...state.columns.slice(typeIndex + 1, state.columns.length),
+              ],
+              skipReset: true,
+            };
+          } else {
+            let options = [];
+            state.data.forEach((row) => {
+              if (row[action.columnId]) {
+                options.push({
+                  label: row[action.columnId],
+                  backgroundColor: randomColor(),
+                });
+              }
+            });
+            return {
+              ...state,
+              columns: [
+                ...state.columns.slice(0, typeIndex),
+                {
+                  ...state.columns[typeIndex],
+                  dataType: action.dataType,
+                  options: [...state.columns[typeIndex].options, ...options],
+                },
+                ...state.columns.slice(typeIndex + 1, state.columns.length),
+              ],
+              skipReset: true,
+            };
+          }
+        case "text":
+          if (state.columns[typeIndex].dataType === "text") {
+            return state;
+          } else if (state.columns[typeIndex].dataType === "select") {
+            return {
+              ...state,
+              skipReset: true,
+              columns: [
+                ...state.columns.slice(0, typeIndex),
+                { ...state.columns[typeIndex], dataType: action.dataType },
+                ...state.columns.slice(typeIndex + 1, state.columns.length),
+              ],
+            };
+          } else {
+            return {
+              ...state,
+              skipReset: true,
+              columns: [
+                ...state.columns.slice(0, typeIndex),
+                { ...state.columns[typeIndex], dataType: action.dataType },
+                ...state.columns.slice(typeIndex + 1, state.columns.length),
+              ],
+              data: state.data.map((row) => ({
+                ...row,
+                [action.columnId]: row[action.columnId] + "",
+              })),
+            };
+          }
+        default:
+          return state;
+      }
+    }
+    case "update_column_header": {
+      const index = state.columns.findIndex((column) => column.id === action.columnId);
+      return {
+        ...state,
+        skipReset: true,
+        columns: [
+          ...state.columns.slice(0, index),
+          { ...state.columns[index], label: action.label },
+          ...state.columns.slice(index + 1, state.columns.length),
+        ],
+      };
+    }
+    case "update_cell":
+      return {
+        ...state,
+        skipReset: true,
+        data: state.data.map((row, index) => {
+          if (index === action.rowIndex) {
+            return {
+              ...state.data[action.rowIndex],
+              [action.columnId]: action.value,
+            };
+          }
+          return row;
+        }),
+      };
+    case "add_column_to_left": {
+      const leftIndex = state.columns.findIndex(
+        (column) => column.id === action.columnId
+      );
+      let leftId = shortId();
+      return {
+        ...state,
+        skipReset: true,
+        columns: [
+          ...state.columns.slice(0, leftIndex),
+          {
+            id: leftId,
+            label: "Column",
+            accessor: leftId,
+            dataType: "text",
+            created: action.focus && true,
+            options: [],
+          },
+          ...state.columns.slice(leftIndex, state.columns.length),
+        ],
+      };
+    }
+    case "add_column_to_right": {
+      const rightIndex = state.columns.findIndex(
+        (column) => column.id === action.columnId
+      );
+      const rightId = shortId();
+      return {
+        ...state,
+        skipReset: true,
+        columns: [
+          ...state.columns.slice(0, rightIndex + 1),
+          {
+            id: rightId,
+            label: "Column",
+            accessor: rightId,
+            dataType: "text",
+            created: action.focus && true,
+            options: [],
+          },
+          ...state.columns.slice(rightIndex + 1, state.columns.length),
+        ],
+      };
+    }
+    case "delete_column": {
+      const deleteIndex = state.columns.findIndex(
+        (column) => column.id === action.columnId
+      );
+      return {
+        ...state,
+        skipReset: true,
+        columns: [
+          ...state.columns.slice(0, deleteIndex),
+          ...state.columns.slice(deleteIndex + 1, state.columns.length),
+        ],
+      };
+    }
+    case "enable_reset":
+      return {
+        ...state,
+        skipReset: false,
+      };
+    default:
+      return state;
+  }
+}
 
 const HallTipSummary = () => {
-  function reducer(state, action) {
-    switch (action.type) {
-      case "add_option_to_column": {
-        const optionIndex = state.columns.findIndex(
-          (column) => column.id === action.columnId
-        );
-        return {
-          ...state,
-          skipReset: true,
-          columns: [
-            ...state.columns.slice(0, optionIndex),
-            {
-              ...state.columns[optionIndex],
-              options: [
-                ...state.columns[optionIndex].options,
-                { label: action.option, backgroundColor: action.backgroundColor },
-              ],
-            },
-            ...state.columns.slice(optionIndex + 1, state.columns.length),
-          ],
-        };
-      }
-      case "add_row":
-        return {
-          ...state,
-          skipReset: true,
-          data: [...state.data, {}],
-        };
-      case "update_column_type": {
-        const typeIndex = state.columns.findIndex(
-          (column) => column.id === action.columnId
-        );
-        switch (action.dataType) {
-          case "number":
-            if (state.columns[typeIndex].dataType === "number") {
-              return state;
-            } else {
-              return {
-                ...state,
-                columns: [
-                  ...state.columns.slice(0, typeIndex),
-                  { ...state.columns[typeIndex], dataType: action.dataType },
-                  ...state.columns.slice(typeIndex + 1, state.columns.length),
-                ],
-                data: state.data.map((row) => ({
-                  ...row,
-                  [action.columnId]: isNaN(row[action.columnId])
-                    ? ""
-                    : Number.parseInt(row[action.columnId]),
-                })),
-              };
-            }
-          case "select":
-            if (state.columns[typeIndex].dataType === "select") {
-              return {
-                ...state,
-                columns: [
-                  ...state.columns.slice(0, typeIndex),
-                  { ...state.columns[typeIndex], dataType: action.dataType },
-                  ...state.columns.slice(typeIndex + 1, state.columns.length),
-                ],
-                skipReset: true,
-              };
-            } else {
-              let options = [];
-              state.data.forEach((row) => {
-                if (row[action.columnId]) {
-                  options.push({
-                    label: row[action.columnId],
-                    backgroundColor: randomColor(),
-                  });
-                }
-              });
-              return {
-                ...state,
-                columns: [
-                  ...state.columns.slice(0, typeIndex),
-                  {
-                    ...state.columns[typeIndex],
-                    dataType: action.dataType,
-                    options: [...state.columns[typeIndex].options, ...options],
-                  },
-                  ...state.columns.slice(typeIndex + 1, state.columns.length),
-                ],
-                skipReset: true,
-              };
-            }
-          case "text":
-            if (state.columns[typeIndex].dataType === "text") {
-              return state;
-            } else if (state.columns[typeIndex].dataType === "select") {
-              return {
-                ...state,
-                skipReset: true,
-                columns: [
-                  ...state.columns.slice(0, typeIndex),
-                  { ...state.columns[typeIndex], dataType: action.dataType },
-                  ...state.columns.slice(typeIndex + 1, state.columns.length),
-                ],
-              };
-            } else {
-              return {
-                ...state,
-                skipReset: true,
-                columns: [
-                  ...state.columns.slice(0, typeIndex),
-                  { ...state.columns[typeIndex], dataType: action.dataType },
-                  ...state.columns.slice(typeIndex + 1, state.columns.length),
-                ],
-                data: state.data.map((row) => ({
-                  ...row,
-                  [action.columnId]: row[action.columnId] + "",
-                })),
-              };
-            }
-          default:
-            return state;
-        }
-      }
-      case "update_column_header": {
-        const index = state.columns.findIndex((column) => column.id === action.columnId);
-        return {
-          ...state,
-          skipReset: true,
-          columns: [
-            ...state.columns.slice(0, index),
-            { ...state.columns[index], label: action.label },
-            ...state.columns.slice(index + 1, state.columns.length),
-          ],
-        };
-      }
-      case "update_cell":
-        return {
-          ...state,
-          skipReset: true,
-          data: state.data.map((row, index) => {
-            if (index === action.rowIndex) {
-              return {
-                ...state.data[action.rowIndex],
-                [action.columnId]: action.value,
-              };
-            }
-            return row;
-          }),
-        };
-      case "add_column_to_left": {
-        const leftIndex = state.columns.findIndex(
-          (column) => column.id === action.columnId
-        );
-        let leftId = shortId();
-        return {
-          ...state,
-          skipReset: true,
-          columns: [
-            ...state.columns.slice(0, leftIndex),
-            {
-              id: leftId,
-              label: "Column",
-              accessor: leftId,
-              dataType: "text",
-              created: action.focus && true,
-              options: [],
-            },
-            ...state.columns.slice(leftIndex, state.columns.length),
-          ],
-        };
-      }
-      case "add_column_to_right": {
-        const rightIndex = state.columns.findIndex(
-          (column) => column.id === action.columnId
-        );
-        const rightId = shortId();
-        return {
-          ...state,
-          skipReset: true,
-          columns: [
-            ...state.columns.slice(0, rightIndex + 1),
-            {
-              id: rightId,
-              label: "Column",
-              accessor: rightId,
-              dataType: "text",
-              created: action.focus && true,
-              options: [],
-            },
-            ...state.columns.slice(rightIndex + 1, state.columns.length),
-          ],
-        };
-      }
-      case "delete_column": {
-        const deleteIndex = state.columns.findIndex(
-          (column) => column.id === action.columnId
-        );
-        return {
-          ...state,
-          skipReset: true,
-          columns: [
-            ...state.columns.slice(0, deleteIndex),
-            ...state.columns.slice(deleteIndex + 1, state.columns.length),
-          ],
-        };
-      }
-      case "enable_reset":
-        return {
-          ...state,
-          skipReset: false,
-        };
-      default:
-        return state;
-    }
-  }
+  const [state, dispatch] = useReducer(reducer, makeData(10));
 
   return (
     <MuiBox sx={{ height: 400, width: "100%" }}>
--- src/util/table.util.ts
+++ src/util/table.util.ts
@@ -1,7 +1,78 @@
+import { faker } from "@faker-js/faker";
+
 export function shortId() {
   return "_" + Math.random().toString(36).substr(2, 9);
 }
 
 export function randomColor() {
   return `hsl(${Math.floor(Math.random() * 360)}, 95%, 90%)`;
+}
+
+export default function makeData(count: number) {
+  let data = [];
+  let options = [];
+  for (let i = 0; i < count; i++) {
+    let row = {
+      ID: faker.string.uuid(),
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      email: faker.internet.email(),
+      age: Math.floor(20 + Math.random() * 20),
+      music: faker.music.genre(),
+    };
+    options.push({ label: row.music, backgroundColor: randomColor() });
+
+    data.push(row);
+  }
+
+  let columns = [
+    {
+      id: "firstName",
+      label: "First Name",
+      accessor: "firstName",
+      minWidth: 100,
+      dataType: "text",
+      options: [],
+    },
+    {
+      id: "lastName",
+      label: "Last Name",
+      accessor: "lastName",
+      minWidth: 100,
+      dataType: "text",
+      options: [],
+    },
+    {
+      id: "age",
+      label: "Age",
+      accessor: "age",
+      width: 80,
+      dataType: "number",
+      options: [],
+    },
+    {
+      id: "email",
+      label: "E-Mail",
+      accessor: "email",
+      width: 300,
+      dataType: "text",
+      options: [],
+    },
+    {
+      id: "music",
+      label: "Music Preference",
+      accessor: "music",
+      dataType: "select",
+      width: 200,
+      options: options,
+    },
+    {
+      id: 999999,
+      width: 20,
+      label: "+",
+      disableResizing: true,
+      dataType: "null",
+    },
+  ];
+  return { columns: columns, data: data, skipReset: false };
 }
