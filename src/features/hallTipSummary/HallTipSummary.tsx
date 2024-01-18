3ad7c259ca6715b9a0383da3c5c4b9945fa5ce10
Rick Park <ricksmp95@gmail.com>
2024-01-18 20:56:27 +0000

    add initial react-table

--- src/features/hallTipSummary/HallTipSummary.tsx
+++ src/features/hallTipSummary/HallTipSummary.tsx
@@ -1,131 +1,227 @@
 import MuiBox from "@mui/material/Box";
-import MuiButton from "@mui/material/Button";
-import {
-  DataGrid,
-  GridActionsCellItem,
-  GridAddIcon,
-  GridColDef,
-  GridDeleteIcon,
-  GridRowId,
-  GridRowModesModel,
-  GridRowsProp,
-  GridToolbarContainer,
-} from "@mui/x-data-grid";
-import { randomId } from "@mui/x-data-grid-generator";
-import { useState } from "react";
 
-const DAYS = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-
-interface EditToolbarProps {
-  setRows: (newRows: (oldRows: GridRowsProp) => GridRowsProp) => void;
-  setRowModesModel: (
-    newModel: (oldModel: GridRowModesModel) => GridRowModesModel
-  ) => void;
-}
+import Table from "@components/table/Table";
 
 const HallTipSummary = () => {
-  const EditToolbar = (props: EditToolbarProps) => {
-    const { setRows } = props;
-
-    const handleClick = () => {
-      const id = randomId();
-      setRows((oldRows) => [
-        ...oldRows,
-        { id, name: "", mon: 0, tue: 0, wed: 0, thu: 0, fri: 0, sat: 0, sun: 0 },
-      ]);
-    };
-
-    return (
-      <GridToolbarContainer>
-        <MuiButton color="primary" onClick={handleClick}>
-          <GridAddIcon />
-        </MuiButton>
-      </GridToolbarContainer>
-    );
-  };
-  const generateColumns = () => {
-    const columns: GridColDef[] = [
-      {
-        field: "name",
-        headerName: "Name",
-        width: 120,
-        editable: true,
-        renderHeader: (params) => {
-          return <b>{params.colDef.headerName}</b>;
-        },
-      },
-    ];
-
-    DAYS.forEach((day) => {
-      columns.push({
-        field: day.toLowerCase(),
-        flex: 1,
-        headerName: day,
-        type: "number",
-        editable: true,
-        sortable: false,
-        headerAlign: "center",
-        align: "center",
-        renderHeader: (params) => {
-          return <b>{params.colDef.headerName}</b>;
-        },
-      });
-    });
-
-    columns.push({
-      field: "actions",
-      type: "actions",
-      width: 20,
-      cellClassName: "actions",
-      getActions: ({ id }) => {
-        return [
-          <GridActionsCellItem
-            icon={<GridDeleteIcon />}
-            label="Delete"
-            onClick={handleDeleteClick(id)}
-            color="inherit"
-          />,
-        ];
-      },
-    });
-
-    return columns;
-  };
-
-  const columns: GridColDef[] = generateColumns();
-
-  const initialRows = [
-    {
-      id: randomId(),
-      name: "John",
-      mon: 1,
-      tue: 2,
-      wed: 3,
-      thu: 4,
-      fri: 5,
-      sat: 6,
-      sun: 7,
-    },
-  ];
-  const [rows, setRows] = useState(initialRows);
-
-  const handleDeleteClick = (id: GridRowId) => () => {
-    setRows(rows.filter((row) => row.id !== id));
-  };
+  function reducer(state, action) {
+    switch (action.type) {
+      case "add_option_to_column": {
+        const optionIndex = state.columns.findIndex(
+          (column) => column.id === action.columnId
+        );
+        return {
+          ...state,
+          skipReset: true,
+          columns: [
+            ...state.columns.slice(0, optionIndex),
+            {
+              ...state.columns[optionIndex],
+              options: [
+                ...state.columns[optionIndex].options,
+                { label: action.option, backgroundColor: action.backgroundColor },
+              ],
+            },
+            ...state.columns.slice(optionIndex + 1, state.columns.length),
+          ],
+        };
+      }
+      case "add_row":
+        return {
+          ...state,
+          skipReset: true,
+          data: [...state.data, {}],
+        };
+      case "update_column_type": {
+        const typeIndex = state.columns.findIndex(
+          (column) => column.id === action.columnId
+        );
+        switch (action.dataType) {
+          case "number":
+            if (state.columns[typeIndex].dataType === "number") {
+              return state;
+            } else {
+              return {
+                ...state,
+                columns: [
+                  ...state.columns.slice(0, typeIndex),
+                  { ...state.columns[typeIndex], dataType: action.dataType },
+                  ...state.columns.slice(typeIndex + 1, state.columns.length),
+                ],
+                data: state.data.map((row) => ({
+                  ...row,
+                  [action.columnId]: isNaN(row[action.columnId])
+                    ? ""
+                    : Number.parseInt(row[action.columnId]),
+                })),
+              };
+            }
+          case "select":
+            if (state.columns[typeIndex].dataType === "select") {
+              return {
+                ...state,
+                columns: [
+                  ...state.columns.slice(0, typeIndex),
+                  { ...state.columns[typeIndex], dataType: action.dataType },
+                  ...state.columns.slice(typeIndex + 1, state.columns.length),
+                ],
+                skipReset: true,
+              };
+            } else {
+              let options = [];
+              state.data.forEach((row) => {
+                if (row[action.columnId]) {
+                  options.push({
+                    label: row[action.columnId],
+                    backgroundColor: randomColor(),
+                  });
+                }
+              });
+              return {
+                ...state,
+                columns: [
+                  ...state.columns.slice(0, typeIndex),
+                  {
+                    ...state.columns[typeIndex],
+                    dataType: action.dataType,
+                    options: [...state.columns[typeIndex].options, ...options],
+                  },
+                  ...state.columns.slice(typeIndex + 1, state.columns.length),
+                ],
+                skipReset: true,
+              };
+            }
+          case "text":
+            if (state.columns[typeIndex].dataType === "text") {
+              return state;
+            } else if (state.columns[typeIndex].dataType === "select") {
+              return {
+                ...state,
+                skipReset: true,
+                columns: [
+                  ...state.columns.slice(0, typeIndex),
+                  { ...state.columns[typeIndex], dataType: action.dataType },
+                  ...state.columns.slice(typeIndex + 1, state.columns.length),
+                ],
+              };
+            } else {
+              return {
+                ...state,
+                skipReset: true,
+                columns: [
+                  ...state.columns.slice(0, typeIndex),
+                  { ...state.columns[typeIndex], dataType: action.dataType },
+                  ...state.columns.slice(typeIndex + 1, state.columns.length),
+                ],
+                data: state.data.map((row) => ({
+                  ...row,
+                  [action.columnId]: row[action.columnId] + "",
+                })),
+              };
+            }
+          default:
+            return state;
+        }
+      }
+      case "update_column_header": {
+        const index = state.columns.findIndex((column) => column.id === action.columnId);
+        return {
+          ...state,
+          skipReset: true,
+          columns: [
+            ...state.columns.slice(0, index),
+            { ...state.columns[index], label: action.label },
+            ...state.columns.slice(index + 1, state.columns.length),
+          ],
+        };
+      }
+      case "update_cell":
+        return {
+          ...state,
+          skipReset: true,
+          data: state.data.map((row, index) => {
+            if (index === action.rowIndex) {
+              return {
+                ...state.data[action.rowIndex],
+                [action.columnId]: action.value,
+              };
+            }
+            return row;
+          }),
+        };
+      case "add_column_to_left": {
+        const leftIndex = state.columns.findIndex(
+          (column) => column.id === action.columnId
+        );
+        let leftId = shortId();
+        return {
+          ...state,
+          skipReset: true,
+          columns: [
+            ...state.columns.slice(0, leftIndex),
+            {
+              id: leftId,
+              label: "Column",
+              accessor: leftId,
+              dataType: "text",
+              created: action.focus && true,
+              options: [],
+            },
+            ...state.columns.slice(leftIndex, state.columns.length),
+          ],
+        };
+      }
+      case "add_column_to_right": {
+        const rightIndex = state.columns.findIndex(
+          (column) => column.id === action.columnId
+        );
+        const rightId = shortId();
+        return {
+          ...state,
+          skipReset: true,
+          columns: [
+            ...state.columns.slice(0, rightIndex + 1),
+            {
+              id: rightId,
+              label: "Column",
+              accessor: rightId,
+              dataType: "text",
+              created: action.focus && true,
+              options: [],
+            },
+            ...state.columns.slice(rightIndex + 1, state.columns.length),
+          ],
+        };
+      }
+      case "delete_column": {
+        const deleteIndex = state.columns.findIndex(
+          (column) => column.id === action.columnId
+        );
+        return {
+          ...state,
+          skipReset: true,
+          columns: [
+            ...state.columns.slice(0, deleteIndex),
+            ...state.columns.slice(deleteIndex + 1, state.columns.length),
+          ],
+        };
+      }
+      case "enable_reset":
+        return {
+          ...state,
+          skipReset: false,
+        };
+      default:
+        return state;
+    }
+  }
 
   return (
     <MuiBox sx={{ height: 400, width: "100%" }}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        slots={{
-          toolbar: EditToolbar,
-        }}
-        slotProps={{
-          toolbar: { setRows },
-        }}
-        disableColumnSelector
-        disableColumnMenu
+      <Table
+        columns={state.columns}
+        data={state.data}
+        dispatch={dispatch}
+        skipReset={state.skipReset}
       />
     </MuiBox>
   );
