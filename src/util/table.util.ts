4a15376d64e46006b1e119a108b969a329138c93
Rick Park <ricksmp95@gmail.com>
2024-01-19 14:25:30 +0000

    update mock data

--- src/util/table.util.ts
+++ src/util/table.util.ts
@@ -10,68 +10,86 @@ export function randomColor() {
 
 export default function makeData(count: number) {
   let data = [];
-  let options = [];
   for (let i = 0; i < count; i++) {
     let row = {
       ID: faker.string.uuid(),
-      firstName: faker.person.firstName(),
-      lastName: faker.person.lastName(),
-      email: faker.internet.email(),
-      age: Math.floor(20 + Math.random() * 20),
-      music: faker.music.genre(),
+      name: faker.person.firstName(),
+      mon: faker.number.int({ min: 0, max: 10 }),
+      tue: faker.number.int({ min: 0, max: 10 }),
+      wed: faker.number.int({ min: 0, max: 10 }),
+      thu: faker.number.int({ min: 0, max: 10 }),
+      fri: faker.number.int({ min: 0, max: 10 }),
+      sat: faker.number.int({ min: 0, max: 10 }),
+      sun: faker.number.int({ min: 0, max: 10 }),
     };
-    options.push({ label: row.music, backgroundColor: randomColor() });
 
     data.push(row);
   }
 
   let columns = [
     {
-      id: "firstName",
-      label: "First Name",
-      accessor: "firstName",
+      id: "name",
+      label: "Name",
+      accessor: "name",
       minWidth: 100,
       dataType: "text",
       options: [],
     },
     {
-      id: "lastName",
-      label: "Last Name",
-      accessor: "lastName",
-      minWidth: 100,
-      dataType: "text",
-      options: [],
-    },
-    {
-      id: "age",
-      label: "Age",
-      accessor: "age",
+      id: "mon",
+      label: "Mon",
+      accessor: "mon",
       width: 80,
       dataType: "number",
       options: [],
     },
     {
-      id: "email",
-      label: "E-Mail",
-      accessor: "email",
-      width: 300,
-      dataType: "text",
+      id: "tue",
+      label: "Tue",
+      accessor: "tue",
+      width: 80,
+      dataType: "number",
       options: [],
     },
     {
-      id: "music",
-      label: "Music Preference",
-      accessor: "music",
-      dataType: "select",
-      width: 200,
-      options: options,
+      id: "wed",
+      label: "Wed",
+      accessor: "wed",
+      width: 80,
+      dataType: "number",
+      options: [],
     },
     {
-      id: 999999,
-      width: 20,
-      label: "+",
-      disableResizing: true,
-      dataType: "null",
+      id: "thu",
+      label: "Thu",
+      accessor: "thu",
+      width: 80,
+      dataType: "number",
+      options: [],
+    },
+    {
+      id: "fri",
+      label: "Fri",
+      accessor: "fri",
+      width: 80,
+      dataType: "number",
+      options: [],
+    },
+    {
+      id: "sat",
+      label: "Sat",
+      accessor: "sat",
+      width: 80,
+      dataType: "number",
+      options: [],
+    },
+    {
+      id: "sun",
+      label: "Sun",
+      accessor: "sun",
+      width: 80,
+      dataType: "number",
+      options: [],
     },
   ];
   return { columns: columns, data: data, skipReset: false };
