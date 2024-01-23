3c2c3be15b31c2c2a61c60a4d88036190cdaaaa8
Rick Park <ricksmp95@gmail.com>
2024-01-23 20:18:28 +0000

    add mouse coordinate hover functionality

--- src/components/table/row/Row.tsx
+++ src/components/table/row/Row.tsx
@@ -1,26 +1,41 @@
 import DeleteIcon from "@mui/icons-material/Delete";
 import { Row as RowType, flexRender } from "@tanstack/react-table";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { HallTipSummaryData } from "../Table.types";
 
 const Row = ({ rowData }: { rowData: RowType<HallTipSummaryData> }) => {
-  const [showDelete, setShowDelete] = useState(false);
+  const [isHovering, setIsHovering] = useState(false);
+
+  const elemId = `tr_${rowData.id}`;
+
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      const container = document.getElementById(elemId);
+      const rect = container?.getBoundingClientRect();
+
+      if (rect) {
+        const isInside = e.clientY >= rect.top && e.clientY <= rect.bottom;
+        setIsHovering(isInside);
+      }
+    };
+
+    document.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, [elemId]);
+
   return (
-    <div key={rowData.id} className="tr">
-      {/* {showDelete && <DeleteIcon />} */}
+    <div key={rowData.id} className="tr" id={elemId}>
+      {isHovering && <DeleteIcon />}
       {rowData.getVisibleCells().map((cell) => (
         <div
           key={cell.id}
           className="td"
           style={{
             width: `calc(var(--col-${cell.column.id}-size) * 1px)`,
-          }}
-          onMouseEnter={() => {
-            setShowDelete(true);
-          }}
-          onMouseLeave={() => {
-            setShowDelete(false);
           }}
         >
           {flexRender(cell.column.columnDef.cell, cell.getContext())}
