8e62097acb38e174cfdc7451c6e139ebecd0b6f3
Rick Park <ricksmp95@gmail.com>
2024-01-25 21:36:26 +0000

    fix delete icon import issue

--- src/components/table/buttons/DeleteButton.tsx
+++ src/components/table/buttons/DeleteButton.tsx
@@ -1,9 +1,9 @@
+import DeleteIcon from "@mui/icons-material/Delete";
 import MuiIconButton, { IconButtonProps } from "@mui/material/IconButton";
-import { GridDeleteIcon as MuiGridDeleteIcon } from "@mui/x-data-grid/material/icons";
 
 const DeleteButton = (props: IconButtonProps) => (
   <MuiIconButton {...props}>
-    <MuiGridDeleteIcon sx={{ fontSize: "18px" }} />
+    <DeleteIcon sx={{ fontSize: "18px" }} />
   </MuiIconButton>
 );
 
